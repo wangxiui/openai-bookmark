84ce2115527ddefa818fc08f250aa83263b54dd4
wangxiui <1036900394@qq.com>
2023-07-08 13:22:34 +0800

    feat: test

--- src/hooks/useOpenai.ts
+++ src/hooks/useOpenai.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 const parseInstruction = async (text: string) => {
   try {
     // 请帮我创建一个111的文件夹
-    const response = await axios.post('/api/completions', {
+    // const response = await axios.post('/api/completions', { // 自动补全模型
+    const response = await axios.post('/api/chat', { // 对话模型
       prompt: `Parse the following instruction for a bookmark manager: "${text}"`,
     });
 
